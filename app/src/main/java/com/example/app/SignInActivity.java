5cce5440e86f04facc5aeb0230b63677001aa53e
Shir-Malka <shirsoftware031@gmail.com>
2022-04-24 12:38:46 +0300

    test

--- app/src/main/java/com/example/app/SignInActivity.java
+++ app/src/main/java/com/example/app/SignInActivity.java
@@ -80,7 +80,7 @@ public class SignInActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                                 Toast.makeText(SignInActivity.this, "User Logged in Successfully", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(getApplicationContext(), AdminMenuAppActivity.class));
+                                startActivity(new Intent(getApplicationContext(), UserMenuAppActivity.class));
 
                         } else {
                             Toast.makeText(SignInActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
