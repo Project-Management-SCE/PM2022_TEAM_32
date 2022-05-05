2e76aedfbe0e9bf4ca6e01f59b97e59772f18276
maze340 <93390529+maze340@users.noreply.github.com>
2022-05-05 21:20:58 +0300

    BSPM2022T32-68: As a owner I can display a list of my mikveh so I can manage them

--- app/src/main/java/com/example/app/MikvehListActivity.java
+++ app/src/main/java/com/example/app/MikvehListActivity.java
@@ -49,7 +49,32 @@ public class MikvehListActivity extends AppCompatActivity {
                         Log.d("Youhou",document.getData().toString());
                         userProfile = document.get("profile").toString();
 
-                        if(userProfile.equals("Admin")) {
+                        if(userProfile.equals("Owner")) {
+                            new FirebaseRealtimeDatabaseHelper().readMikvehByOwner(new FirebaseRealtimeDatabaseHelper.DataStatus() {
+                                @Override
+                                public void DataIsLoaded(List<Mikveh> mikvot, List<String> keys) {
+                                    findViewById(R.id.loading_mikvot).setVisibility(View.GONE);
+                                    new RecyclerView_Config().setConfig(mRecyclerView,
+                                            MikvehListActivity.this, mikvot, keys);
+                                }
+
+                                @Override
+                                public void DataIsInserted() {
+
+                                }
+
+                                @Override
+                                public void DataIsUpdated() {
+
+                                }
+
+                                @Override
+                                public void DataIsDeleted() {
+
+                                }
+                            });
+                        }
+                        else if(userProfile.equals("Admin")) {
                             new FirebaseRealtimeDatabaseHelper().readMikvehDB(new FirebaseRealtimeDatabaseHelper.DataStatus() {
                                 @Override
                                 public void DataIsLoaded(List<Mikveh> mikvot, List<String> keys) {
@@ -75,33 +100,7 @@ public class MikvehListActivity extends AppCompatActivity {
                                 }
                             });
                         }
-                        else {
-                            String city = "אופקים";
-                            new FirebaseRealtimeDatabaseHelper().readMikvehByPosition(city, new FirebaseRealtimeDatabaseHelper.DataStatus() {
-                                @Override
-                                public void DataIsLoaded(List<Mikveh> mikvot, List<String> keys) {
-                                    findViewById(R.id.loading_mikvot).setVisibility(View.GONE);
-                                    new RecyclerView_Config().setConfig(mRecyclerView,
-                                            MikvehListActivity.this, mikvot, keys);
-                                }
-
-                                @Override
-                                public void DataIsInserted() {
-
-                                }
-
-                                @Override
-                                public void DataIsUpdated() {
-
-                                }
-
-                                @Override
-                                public void DataIsDeleted() {
-
-                                }
-                            });
-
-                        }
+                       
 
                     }
                     else {
