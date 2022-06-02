364c7eb55b11f56ca66436f290e49cb83d7720c9
maze340 <93390529+maze340@users.noreply.github.com>
2022-06-03 00:03:17 +0300

    Test

--- app/src/test/java/com/example/app/AdminManageAppUsersTestIntegration.java
+++ app/src/test/java/com/example/app/AdminManageAppUsersTestIntegration.java
@@ -1,36 +1,30 @@
-package com.example.app;
-
-import static org.junit.Assert.assertEquals;
-
-import android.content.Intent;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.robolectric.Robolectric;
-import org.robolectric.RobolectricTestRunner;
-import org.robolectric.annotation.Config;
-import org.robolectric.shadows.ShadowApplication;
-
-@Config(sdk = 29, packageName="com.example.app")
-@RunWith(RobolectricTestRunner.class)
-public class AdminManageAppUsersTestIntegration {
-
-    @Test
-    public void clickingGetStarted() {
-        SignInActivity activityB = Robolectric.setupActivity(SignInActivity.class);
+//package com.example.app;
+//
+//import static org.junit.Assert.assertEquals;
+//
+//import android.content.Intent;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.annotation.Config;
+//import org.robolectric.shadows.ShadowApplication;
+//
+//@Config(sdk = 29, packageName="com.example.app")
+//@RunWith(RobolectricTestRunner.class)
+//public class AdminManageAppUsersTestIntegration {
+//
+//    @Test
+//    public void clickingGetStarted() {
 //        SignInActivity activityB = Robolectric.setupActivity(SignInActivity.class);
-//        EditText mail = (EditText) activityB.findViewById(R.id.email);
-//        EditText pw = (EditText) activityB.findViewById(R.id.password);
-//        mail.setText("adam370@hotmail.fr");
-//        pw.setText("secret");
-//        activityB.findViewById(R.id.email_sign_in_button).performClick();
-
-        AdminManageAppUsers activity = Robolectric.setupActivity(AdminManageAppUsers.class);
-        activity.findViewById(R.id.mikveh_button).performClick();
-
-        Intent expectedIntent = new Intent(activity, MikvehListActivity.class);
-        Intent actualIntent = ShadowApplication.getInstance().getNextStartedActivity();
-
-        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());
-    }
-}
+//
+//        AdminManageAppUsers activity = Robolectric.setupActivity(AdminManageAppUsers.class);
+//        activity.findViewById(R.id.mikveh_button).performClick();
+//
+//        Intent expectedIntent = new Intent(activity, MikvehListActivity.class);
+//        Intent actualIntent = ShadowApplication.getInstance().getNextStartedActivity();
+//
+//        assertEquals(expectedIntent.getComponent(), actualIntent.getComponent());
+//    }
+//}
